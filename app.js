dbff5106db737d8e99bdaf3be6813eef342d6310
EugeSorgetti <eugesorgetti@gmail.com>
2021-06-15 13:41:52 -0300

    Fotos, galeria con Javascript y Grids, skills

---
+++ app.js
@@ -0,0 +1,17 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content');
+console.log(tabContents);
+
+tabs.forEach (tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        });
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        target.classList.add('active');
+        tab.classList.add('active');
+    })
+})
