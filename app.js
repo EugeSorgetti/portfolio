1a70692484b4908f1d689f9d817e04b6957c3af0
EugeSorgetti <eugesorgetti@gmail.com>
2021-06-24 18:36:39 -0300

    Lightbox con Javascript

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content');
-console.log(tabContents);
 
 tabs.forEach (tab => {
     tab.addEventListener('click', () => {
@@ -14,4 +13,22 @@ tabs.forEach (tab => {
         target.classList.add('active');
         tab.classList.add('active');
     })
+})
+
+const thumbnails = document.querySelectorAll('img[data-type]');
+const lightbox = document.getElementById('lightbox');
+const closeBtn = document.getElementById('close-btn');
+
+console.log(thumbnails.src);
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const image = document.getElementById('lightbox-image');
+        image.src = thumbnail.src;
+    })
+});
+
+closeBtn.addEventListener('click', e => {
+    lightbox.classList.remove('active');
 })
